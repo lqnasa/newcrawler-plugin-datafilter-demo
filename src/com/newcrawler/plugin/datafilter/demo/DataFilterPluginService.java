ade506d12a07dcfe1974774a74fe6268cabf94ff
speed <k78@qq.com>
2015-12-06 23:45:14 +0800

    重新设计接口

--- src/com/newcrawler/plugin/datafilter/demo/DataFilterPluginService.java
+++ src/com/newcrawler/plugin/datafilter/demo/DataFilterPluginService.java
@@ -2,10 +2,15 @@ package com.newcrawler.plugin.datafilter.demo;
 
 import java.util.Map;
 
+import com.soso.plugin.bo.DataFilterPluginBo;
+
 public class DataFilterPluginService implements com.soso.plugin.DataFilterPlugin{
 
 	@Override
-	public String execute(Map<String, String> properties, final String value, final Map<String, String> vMap) {
+	public String execute(final DataFilterPluginBo dataFilterPluginBo) {
+		Map<String, String> properties=dataFilterPluginBo.getProperties();
+		final String value=dataFilterPluginBo.getValue();
+		final Map<String, String> vMap=dataFilterPluginBo.getvMap();
 		
 		return "数据过滤插件-DEMO.";
 	}
